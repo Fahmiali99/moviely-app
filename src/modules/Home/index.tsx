dfc0a3e88f8f5ef5843c34b326957ed02a7c90cf
Fahmi <fahmiali130499@gmail.com>
2023-05-20 19:11:58 +0700

    update server

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "@/store"; // Assuming you have a RootState type defined in your store
+import { RootState } from "@/store";
 import { getPopular } from "@/lib/Popular/fetchApi";
 import { setPopular } from "@/store/popular";
 
 function HomePage() {
   const dispatch = useDispatch();
-  const { popular } = useSelector((state: RootState) => state.popular); // Make sure to specify the RootState type
+  const { popular } = useSelector((state: RootState) => state.popular);
   useEffect(() => {
     getPopular().then((data) => {
       dispatch(setPopular(data));
